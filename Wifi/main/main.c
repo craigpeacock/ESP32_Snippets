96f67b95b004c06b1eceb6d4b6c5d5b81f12cb4c
Craig Peacock <craigp@beyondlogic.org>
2023-09-29 15:39:29 +0930

    Add WiFi Station example with IPv6 SLAAC Support

---
+++ Wifi/main/main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "wifi.h"
+
+static const char *TAG = "main";
+
+void app_main(void)
+{
+	ESP_LOGI(TAG, "ESP32 WiFi Example");
+
+	// Initialise network
+	wifi_init_sta();
+
+	while(1) {
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
